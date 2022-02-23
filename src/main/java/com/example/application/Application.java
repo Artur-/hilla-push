4ec87d79f1551528300af1f6af30dabef4e571d3
Artur Signell <artur@vaadin.com>
2022-02-23 18:46:35 +0200

    Remove extra import

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,7 +1,6 @@
 package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.server.PWA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
